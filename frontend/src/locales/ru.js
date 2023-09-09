713e61925f51cbf1b344b2705d935a0d0bce72cb
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-09-09 14:46:53 +0400

    change locales ru file

--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -1,1 +1,30 @@
-export default {};
+export default {
+  translation: {
+    navbar: {
+      hexletChat: 'Hexlet Chat',
+      logout: 'Выйти',
+    },
+    forms: {
+      invalidTooltips: {
+        requiredField: 'Обязательное поле',
+        requiredCharactersNumber: 'От 3 до 20 символов',
+      },
+      registration: 'Регистрация',
+      password: 'Пароль',
+    },
+    loginForm: {
+      login: 'Войти',
+      yourNickname: 'Ваш ник',
+      invalidUsernameOrPassword: 'Неверные имя пользователя или пароль',
+      noAccount: 'Нет аккаунта? ',
+    },
+    signupForm: {
+      minSixСharacters: 'Не менее 6 символов',
+      passwordsMustMatch: 'Пароли должны совпадать',
+      username: 'Имя пользователя',
+      confirmPassword: 'Подтвердите пароль',
+      signup: 'Зарегистрироваться',
+    },
+    channels: 'Каналы',
+  },
+};
