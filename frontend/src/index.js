3c8d7b93b0d3d0a501f8a07300d5abc732b61db6
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-09-16 12:36:55 +0400

    fix index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import init from './init.jsx';
 
 const app = async () => {
-  const root = ReactDOM.createRoot(document.getElementById('chat'));
-  root.render(await init());
+  const root = ReactDOM.createRoot(document.querySelector('#chat'));
+  const vdom = await init();
+  root.render(<React.StrictMode>{vdom}</React.StrictMode>);
 };
 
 app();
