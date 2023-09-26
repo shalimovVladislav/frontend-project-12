cc99378cd5d1363bc2b6b718af01e0db0a08bfd0
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-09-26 18:31:20 +0400

    fix slices index.js

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -2,13 +2,20 @@ import { combineReducers, createSelector } from '@reduxjs/toolkit';
 
 import channelsReducer, { actions as channelsActions, selectors as channelsSelectors } from './channelsReducer.js';
 import messagesReducer, { actions as messagesActions, selectors as messagesSelectors } from './messagesReducer.js';
+import modal, { actions as modalActions } from './modal.js';
 
 const getCurrentChannelId = (state) => state.channels.currentChannelId;
+
 const getCurrentChannel = (state) => {
   const currentChannelId = getCurrentChannelId(state);
   const currentChannel = channelsSelectors.selectById(state, currentChannelId);
   return currentChannel;
 };
+
+const getChannelsNames = createSelector([
+  channelsSelectors.selectAll,
+], (channels) => channels.map((c) => c.name));
+
 const getMessagesForCurrentChannel = createSelector([
   getCurrentChannelId,
   messagesSelectors.selectAll,
@@ -22,15 +29,18 @@ export const selectors = {
   messages: messagesSelectors,
   getCurrentChannelId,
   getCurrentChannel,
+  getChannelsNames,
   getMessagesForCurrentChannel,
 };
 
 export const actions = {
   ...channelsActions,
   ...messagesActions,
+  ...modalActions,
 };
 
 export default combineReducers({
   channels: channelsReducer,
   messages: messagesReducer,
+  modal,
 });
